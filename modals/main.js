dff04399dd630621be4e958fa74793f943c97dfb
Willian Justen <willianjustenqui@gmail.com>
2018-05-19 17:30:50 -0300

    Add Modal FadeInScale

--- modals/main.js
+++ modals/main.js
@@ -1,17 +1,27 @@
 // Modal Elements
 const modal = document.querySelector('.modal');
+const modalBox = document.querySelector('.modal__box');
 
 // Buttons
 const btnClose = document.querySelector('.modal__close');
 const btnFadeIn = document.getElementById('fade-in');
+const btnFadeInScale = document.getElementById('fade-in-scale');
+
 
 // Events
 btnClose.addEventListener('click', close);
 btnFadeIn.addEventListener('click', fadeIn);
+btnFadeInScale.addEventListener('click', fadeInScale)
 
 // Animation Functions
 function fadeIn() {
     TweenMax.to(modal, .5, { autoAlpha: 1 });
+}
+
+function fadeInScale() {
+    TweenMax.set(modalBox, { scale: 0 });
+    TweenMax.to(modal, .5, { autoAlpha: 1 });
+    TweenMax.to(modalBox, 1, { scale: 1, delay: .5, ease: Elastic.easeOut });
 }
 
 function close() {
