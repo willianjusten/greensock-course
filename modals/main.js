644c60b7fe1da077e5afcf961bf219eaba525428
Willian Justen <willianjustenqui@gmail.com>
2018-05-19 15:36:15 -0300

    Add modal close

--- modals/main.js
+++ modals/main.js
@@ -1,8 +1,14 @@
 const modal = document.querySelector('.modal');
 const btnFadeIn = document.getElementById('fadeIn');
+const btnClose = document.querySelector('.modal__close');
 
 btnFadeIn.addEventListener('click', fadeIn);
+btnClose.addEventListener('click', close);
 
 function fadeIn() {
     TweenMax.to(modal, .5, { autoAlpha: 1 });
+}
+
+function close() {
+    TweenMax.to(modal, .5, { autoAlpha: 0 });
 }
