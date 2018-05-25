045b648102a51c2537d6aa8310c34dc08d5700cd
Willian Justen <willianjustenqui@gmail.com>
2018-05-25 17:49:09 -0300

    Add modal slideIn

--- modals/main.js
+++ modals/main.js
@@ -6,12 +6,13 @@ const modalBox = document.querySelector('.modal__box');
 const btnClose = document.querySelector('.modal__close');
 const btnFadeIn = document.getElementById('fade-in');
 const btnFadeInScale = document.getElementById('fade-in-scale');
-
+const btnSlideIn = document.getElementById('slide-in');
 
 // Events
 btnClose.addEventListener('click', close);
 btnFadeIn.addEventListener('click', fadeIn);
-btnFadeInScale.addEventListener('click', fadeInScale)
+btnFadeInScale.addEventListener('click', fadeInScale);
+btnSlideIn.addEventListener('click', slideIn);
 
 // Animation Functions
 function fadeIn() {
@@ -22,6 +23,11 @@ function fadeInScale() {
     TweenMax.set(modalBox, { scale: 0 });
     TweenMax.to(modal, .5, { autoAlpha: 1 });
     TweenMax.to(modalBox, 1, { scale: 1, delay: .5, ease: Elastic.easeOut });
+}
+
+function slideIn() {
+    TweenMax.to(modal, .5, { autoAlpha: 1 });
+    TweenMax.from(modalBox, .5, { autoAlpha: 0, left: -300, top: 200 });
 }
 
 function close() {
