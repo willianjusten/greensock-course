6aac2c4c3179bc8be207ccb7def9dbf60d11a901
Guilherme Louro <guilherme-louro@hotmail.com>
2018-06-01 21:47:09 -0300

    Add fullscreen

--- modals/main.js
+++ modals/main.js
@@ -1,18 +1,23 @@
 // Modal Elements
 const modal = document.querySelector('.modal');
 const modalBox = document.querySelector('.modal__box');
+const modalHeader = document.querySelector('.modal__header');
+const modalTitle = document.querySelector('.modal__title');
+const modalClose = document.querySelector('.modal__close');
 
 // Buttons
 const btnClose = document.querySelector('.modal__close');
 const btnFadeIn = document.getElementById('fade-in');
 const btnFadeInScale = document.getElementById('fade-in-scale');
 const btnSlideIn = document.getElementById('slide-in');
+const btnFullScreen = document.getElementById('fullscreen');
 
 // Events
 btnClose.addEventListener('click', close);
 btnFadeIn.addEventListener('click', fadeIn);
 btnFadeInScale.addEventListener('click', fadeInScale);
 btnSlideIn.addEventListener('click', slideIn);
+btnFullScreen.addEventListener('click', fullScreen);
 
 // Animation Functions
 function fadeIn() {
@@ -26,8 +31,16 @@ function fadeInScale() {
 }
 
 function slideIn() {
-    TweenMax.to(modal, .5, { autoAlpha: 1 });
-    TweenMax.from(modalBox, .5, { autoAlpha: 0, left: -300, top: 200 });
+    TweenMax.to(modal, 0.5, { autoAlpha: 1 });
+    TweenMax.from(modalBox, 0.5, { autoAlpha: 0, top: 300 });
+}
+
+function fullScreen() {
+    TweenMax.set(modal, { backgroundColor: '#fff', scale: 0 });
+    TweenMax.set(modalClose, { color: '#333' });
+    TweenMax.set([modalHeader, modalBox], { position: 'initial' });
+    TweenMax.set(modalTitle, { color: '#333', backgroundColor: '#fff' });
+    TweenMax.to(modal, .2, { autoAlpha: 1, scale: 1 });
 }
 
 function close() {
