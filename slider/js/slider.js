ceb65b95fb37ff145aac577f353c3b4a34e8c1a7
Guilherme Louro <guilherme-louro@hotmail.com>
2018-08-02 01:59:40 -0300

    Refactor first animation

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,13 +1,20 @@
 // Selectors
-
 const sliderItems = document.querySelectorAll('.slider__item');
 
-TweenMax.set(sliderItems[0], { scale: .9 });
+// Slider
+const Slider = {
+    in: (index) => {
+        const item = sliderItems[index];
+        const texts = item.querySelectorAll('p')
+        const timeline = new TimelineMax();
+        
+        TweenMax.set(item, { scale: .9 });
+        timeline
+            .to(item, .5, { left: 0, delay: 1 })
+            .to(item, .5, { scale: 1 })
+            .staggerFrom(texts, .5, { y: 300, autoAlpha: 0, ease: Back.easeOut }, 0.2)
+    },
+    out: () => {},
+}
 
-const texts = sliderItems[0].querySelectorAll('p')
-const timeline = new TimelineMax();
-
-timeline
-    .to(sliderItems[0], .5, { left: 0, delay: 1 })
-    .to(sliderItems[0], .5, { scale: 1 })
-    .staggerFrom(texts, .5, { y: 300, autoAlpha: 0, ease: Back.easeOut }, 0.2)
+Slider.in(0);
