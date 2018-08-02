23979c8339cc91e75bf773a4a999e12dcbb4abbf
Guilherme Louro <guilherme-louro@hotmail.com>
2018-08-02 02:16:14 -0300

    out

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -14,7 +14,15 @@ const Slider = {
             .to(item, .5, { scale: 1 })
             .staggerFrom(texts, .5, { y: 300, autoAlpha: 0, ease: Back.easeOut }, 0.2)
     },
-    out: () => {},
+    out: (index) => {
+        const item = sliderItems[index];
+        const texts = item.querySelectorAll('p')
+        const timeline = new TimelineMax();
+        timeline
+            .staggerTo(texts, .5, { y: 300, autoAlpha: 0, ease: Back.easeIn }, '-0.5')
+            .to(item, .5, { scale: .9 })
+            .to(item, .5, { left: '100vw' })
+    },
 }
 
 Slider.in(0);
